cec0e3462c21185b45299a8fca77aa450b405ae1
x3mxray <x3m.xray@gmail.com>
2023-03-04 22:00:17 +0300

    serialization

--- src/Feature/ContentBuilder/Code/Commands/AutoGenerateContentCommand.cs
+++ src/Feature/ContentBuilder/Code/Commands/AutoGenerateContentCommand.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using EditorsCopilot.Foundation.OpenAI.Core;
-using EditorsCopilot.Foundation.OpenAI.Core.Core.Models;
+//using EditorsCopilot.Foundation.OpenAI.Core;
+//using EditorsCopilot.Foundation.OpenAI.Core.Core.Models;
 using Sitecore;
 using Sitecore.Data;
 using Sitecore.Data.Fields;
@@ -41,13 +41,13 @@ namespace EditorsCopilot.Feature.ContentBuilder.Core.Commands
                         var item = Database.GetItem(uri);
                         var topic = args.Result;
                         Log.Info($"Generate content for item '{item.ID}' and topic '{args.Result}'", this);
-                        var credentials = new OpenAiCredentials()
-                        {
-                            ApiToken = "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",
-                        };
+                        //var credentials = new OpenAiCredentials()
+                        //{
+                        //    ApiToken = "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",
+                        //};
 
-                        var api = new OpenAPI(credentials);
-                        Log.Info($"OpenAI result: title = '{api.TextService.GetTitle(topic)}'", this);
+                        //var api = new OpenAPI(credentials);
+                        //Log.Info($"OpenAI result: title = '{api.TextService.GetTitle(topic)}'", this);
                         Context.ClientPage.SendMessage(this, "item:load(id=" + item.ID + ")");
                     }
                 }
